2850ee8bb35bf7d5dbc639220dbb4934d1e572e5
michal <stachnikmichal@gmail.com>
2023-08-31 23:37:12 +0000

    created a simple-server

---
+++ app.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+http.createServer(function(req, res){
+res.write('welcome to my simple server');
+res.end();
+}).listen(3000);
+
+console.log('server started on 3000');
